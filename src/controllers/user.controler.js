f4265cb6ff49f574b7eb334887db956c45a14a3a
Naman <namankumar62039@gmail.com>
2023-12-19 00:27:16 +0530

    handling data from postman and validating data

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -1,9 +1,37 @@
 import {asyncHeandler} from "../utils/asyncHeandler.js";
+import {ApiError} from "../utils/apiError.js";
+import  {User}  from "../models/User.Model.js";
 
 const registerUser = asyncHeandler( async ( req, res ) => {
-    res.status(200).json({
-        message: "Backend With Chai Or Code"
+    // git user details from frontend
+    //validation not empty
+    //check if user already exist (username or email)
+    // check for images specially for avtar
+    //Upload them on cloudinary avtar
+    //create user object - create entry in data base
+    //remove password and refresh token field  from responce
+    //check for user creation 
+    //return responce
+
+    const {userName, email, fullname, password} = req.body;
+    console.log(`Email: ${email}`, `UserName: ${userName}`, `Fullname: ${fullname}`, `Password: ${password}`);
+
+    if (
+        [userName, email, fullname, password].some((field) => field.trim() === "")
+    ) {
+        throw new ApiError(400, "All Field Are Required")
+    };
+
+    if (!email.includes("@")) throw new ApiError;
+
+
+    const exestingUser = User.findOne({
+        $or: [{userName}, {email}]
     })
+
+    if(exestingUser) {
+        throw new ApiError(409, "User With Email Or User Name Already Exiest")
+    }
 } )
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controler.js";
+import {upload} from "../middlewares/Multer.MiddleWare.js"
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // injecting middleware from multer
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+
+        {
+            name: "coverimage",
+            maxCount: 1
+        }
+    ]),
+
+    registerUser
+    )
 
 export default router;
